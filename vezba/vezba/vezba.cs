6102dab804f6aee5331200942e9bf79a855c3997
jocololika1999@gmail.com <jocololika1999@gmail.com>
2020-11-21 15:16:34 +0100

    Kreirana model klasa

---
+++ vezba/vezba/vezba.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vezba
+{
+    class vezba
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+
+        public int Points { get; set; }
+
+
+        public vezba(int Id, string StudentName, string StudentIndex, int Points)
+        {
+            this.Id = Id;
+            this.StudentName = StudentName;
+            this.StudentIndex = StudentIndex;
+            this.Points = Points;
+        }
+
+        public override string ToString()
+        {
+            return "Ime: " + this.StudentName + "\n" + 
+                "Index" + this.StudentIndex + "\n"  +
+                "Poeni:" + this.Points;
+        }
+    }
+}
